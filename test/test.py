34d7cc22e03c0947ec7828a6ef0c514082a8541a
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-11-09 09:43:18 +0000

    Passes 3 bit majority funciton, to revert back to multiple tests later

--- test/test.py
+++ test/test.py
@@ -8,7 +8,8 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def main_test(dut):
     print(dir(dut))
-    seeds = {"not": 0b01010101, "and2": 0b10001000, "or2": 0b11101110, "xor2": 0b01100110, "nand2": 0b01110111, "nor2": 0b00010001, "nand3": 0b01111111, "nor3": 0b00000001}
+    seeds = {"not": 0b01010101, "and2": 0b10001000, "or2": 0b11101110, "xor2": 0b01100110, "nand2": 0b01110111, "nor2": 0b00010001, "nand3": 0b01111111, "nor3": 0b00000001,
+            "majority": 0b11101000}
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -28,6 +29,7 @@ async def main_test(dut):
     await test_a_b_nor_async(dut, seeds["nor2"])
     await test_a_b_c_nand_async(dut,seeds["nand3"])
     await test_a_b_c_nor_async(dut,seeds["nor3"])
+    await test_a_b_c_majority_async(dut,seeds["majority"])
     dut._log.info("All Tests Completed Successfully!")
 
 async def test_a_not_async(dut,seed):
@@ -42,7 +44,7 @@ async def test_a_not_async(dut,seed):
         dut.ui_in.value = input_value
         a_value = input_value & 0b00000001
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"not A:{bin(a_value)}")
+        dut._log.info(f"A:{bin(a_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~a_value & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
@@ -61,7 +63,7 @@ async def test_a_b_and_async(dut,seed):
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} and B:{bin(b_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == ((a_value & b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
@@ -79,7 +81,7 @@ async def test_a_b_or_async(dut, seed):
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} or B:{bin(b_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == ((a_value | b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
@@ -97,7 +99,7 @@ async def test_a_b_xor_async(dut,seed):
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} xor B:{bin(b_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == ((a_value ^ b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
@@ -115,7 +117,7 @@ async def test_a_b_nand_async(dut,seed):
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} nand B:{bin(b_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value & b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
@@ -133,7 +135,7 @@ async def test_a_b_nor_async(dut,seed):
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} nor B:{bin(b_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value | b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
@@ -152,7 +154,7 @@ async def test_a_b_c_nand_async(dut,seed):
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} nand B:{bin(b_value)} nand C:{bin(c_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value & b_value & c_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
@@ -171,7 +173,26 @@ async def test_a_b_c_nor_async(dut,seed):
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
         await ClockCycles(dut.clk,1)
-        dut._log.info(f"A:{bin(a_value)} nor B:{bin(b_value)} nor C:{bin(c_value)}")
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value | b_value | c_value) & 0b00000001)
+        input_value += 1
+    await ClockCycles(dut.clk,1)
+
+async def test_a_b_c_majority_async(dut,seed):
+    dut._log.info("Start Test: 3 Bit Majority (Async)")
+    dut.uio_in.value = seed
+    dut.ui_in.value = 0b00001000
+    await ClockCycles(dut.clk,1)
+    dut.ui_in.value = 0b00000000
+    await ClockCycles(dut.clk,1)
+    input_value = 0
+    while(input_value != 8):
+        dut.ui_in.value = input_value
+        a_value = input_value & 0b00000001
+        b_value = (input_value & 0b00000010) >> 1
+        c_value = (input_value & 0b00000100) >> 2
+        await ClockCycles(dut.clk,1)
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
+        assert dut.uo_out.value == (((a_value & b_value) | (c_value & (a_value | b_value)) ) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
