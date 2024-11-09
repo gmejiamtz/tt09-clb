258bbdb20f7218896d792111c7da9c4496868c56
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-11-09 10:58:54 +0000

    Added all async tests dont know how to random functions but will add synchronicity later

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ from cocotb.triggers import ClockCycles
 async def main_test(dut):
     print(dir(dut))
     seeds = {"not": 0b01010101, "and2": 0b10001000, "or2": 0b11101110, "xor2": 0b01100110, "nand2": 0b01110111, "nor2": 0b00010001, "nand3": 0b01111111, "nor3": 0b00000001,
-            "majority": 0b11101000}
+            "majority": 0b11101000, "even_parity": 0b01101001,"one_hot":0b00010110}
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -30,6 +30,8 @@ async def main_test(dut):
     await test_a_b_c_nand_async(dut,seeds["nand3"])
     await test_a_b_c_nor_async(dut,seeds["nor3"])
     await test_a_b_c_majority_async(dut,seeds["majority"])
+    await test_a_b_c_even_parity_async(dut,seeds["even_parity"])
+    await test_a_b_c_one_hot_async(dut,seeds["one_hot"])
     dut._log.info("All Tests Completed Successfully!")
 
 async def test_a_not_async(dut,seed):
@@ -194,5 +196,43 @@ async def test_a_b_c_majority_async(dut,seed):
         await ClockCycles(dut.clk,1)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (((a_value & b_value) | (c_value & (a_value | b_value)) ) & 0b00000001)
+        input_value += 1
+    await ClockCycles(dut.clk,1)
+
+async def test_a_b_c_even_parity_async(dut,seed):
+    dut._log.info("Start Test: 3 Bit Even Parity (Async)")
+    dut.uio_in.value = seed
+    dut.ui_in.value = 0b00001000
+    await ClockCycles(dut.clk,1)
+    dut.ui_in.value = 0b00000000
+    await ClockCycles(dut.clk,1)
+    input_value = 0
+    while(input_value != 8):
+        dut.ui_in.value = input_value
+        a_value = input_value & 0b00000001
+        b_value = (input_value & 0b00000010) >> 1
+        c_value = (input_value & 0b00000100) >> 2
+        await ClockCycles(dut.clk,1)
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
+        assert dut.uo_out.value == (~(a_value ^ b_value ^ c_value) & 0b00000001)
+        input_value += 1
+    await ClockCycles(dut.clk,1)
+
+async def test_a_b_c_one_hot_async(dut,seed):
+    dut._log.info("Start Test: 3 Bit One Hot Detector (Async)")
+    dut.uio_in.value = seed
+    dut.ui_in.value = 0b00001000
+    await ClockCycles(dut.clk,1)
+    dut.ui_in.value = 0b00000000
+    await ClockCycles(dut.clk,1)
+    input_value = 0
+    while(input_value != 8):
+        dut.ui_in.value = input_value
+        a_value = input_value & 0b00000001
+        b_value = (input_value & 0b00000010) >> 1
+        c_value = (input_value & 0b00000100) >> 2
+        await ClockCycles(dut.clk,1)
+        dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
+        assert dut.uo_out.value == (((a_value & ~b_value & ~c_value) | (~a_value & b_value & ~c_value) | (~a_value & ~b_value & c_value)) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
