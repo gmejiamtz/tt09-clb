723dda77279b412f0638fe49a3b94c3e9ced7be6
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2024-11-10 02:18:20 +0000

    Added synchronous boolean arg to all test coroutines to change ui_in input to force the clb to read out the dff and account for proper cycle delay in the tb

--- test/test.py
+++ test/test.py
@@ -21,236 +21,250 @@ async def main_test(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    await test_a_not_async(dut,seeds["not"])
-    await test_a_b_and_async(dut,seeds["and2"])
-    await test_a_b_or_async(dut,seeds["or2"])
-    await test_a_b_xor_async(dut,seeds["xor2"])
-    await test_a_b_nand_async(dut,seeds["nand2"])
-    await test_a_b_nor_async(dut, seeds["nor2"])
-    await test_a_b_c_nand_async(dut,seeds["nand3"])
-    await test_a_b_c_nor_async(dut,seeds["nor3"])
-    await test_a_b_c_majority_async(dut,seeds["majority"])
-    await test_a_b_c_even_parity_async(dut,seeds["even_parity"])
-    await test_a_b_c_one_hot_async(dut,seeds["one_hot"])
-    await test_a_not_sync(dut,seeds["not"])
+    await test_a_not(dut,seeds["not"])
+    await test_a_b_and(dut,seeds["and2"])
+    await test_a_b_or(dut,seeds["or2"])
+    await test_a_b_xor(dut,seeds["xor2"])
+    await test_a_b_nand(dut,seeds["nand2"])
+    await test_a_b_nor(dut, seeds["nor2"])
+    await test_a_b_c_nand(dut,seeds["nand3"])
+    await test_a_b_c_nor(dut,seeds["nor3"])
+    await test_a_b_c_majority(dut,seeds["majority"])
+    await test_a_b_c_even_parity(dut,seeds["even_parity"])
+    await test_a_b_c_one_hot(dut,seeds["one_hot"])
+    await test_a_not(dut,seeds["not"],True)
+    await test_a_b_and(dut,seeds["and2"],True)
+    await test_a_b_or(dut,seeds["or2"],True)
+    await test_a_b_xor(dut,seeds["xor2"],True)
+    await test_a_b_nand(dut,seeds["nand2"],True)
+    await test_a_b_nor(dut, seeds["nor2"],True)
+    await test_a_b_c_nand(dut,seeds["nand3"],True)
+    await test_a_b_c_nor(dut,seeds["nor3"],True)
+    await test_a_b_c_majority(dut,seeds["majority"],True)
+    await test_a_b_c_even_parity(dut,seeds["even_parity"],True)
+    await test_a_b_c_one_hot(dut,seeds["one_hot"],True)
     dut._log.info("All Tests Completed Successfully!")
 
-async def test_a_not_async(dut,seed):
-    dut._log.info("Start Test: not A (Async)")
+async def test_a_not(dut,seed, sync=False):
+    dut._log.info(f"Start Test: not A | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~a_value & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
 
-
-async def test_a_b_and_async(dut,seed):
-    dut._log.info("Start Test: A and B (Async)")
+async def test_a_b_and(dut,seed,sync=False):
+    dut._log.info(f"Start Test: A and B | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == ((a_value & b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
 
-async def test_a_b_or_async(dut, seed):
-    dut._log.info("Start Test: A or B (Async)")
+async def test_a_b_or(dut, seed,sync=False):
+    dut._log.info(f"Start Test: A or B | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == ((a_value | b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
 
-async def test_a_b_xor_async(dut,seed):
-    dut._log.info("Start Test: A xor B (Async)")
+async def test_a_b_xor(dut,seed,sync=False):
+    dut._log.info(f"Start Test: A xor B | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == ((a_value ^ b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,2)
 
-async def test_a_b_nand_async(dut,seed):
-    dut._log.info("Start Test: A nand B (Async)")
+async def test_a_b_nand(dut,seed,sync=False):
+    dut._log.info(f"Start Test: A nand B | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value & b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
 
-async def test_a_b_nor_async(dut,seed):
-    dut._log.info("Start Test: A nor B (Async)")
+async def test_a_b_nor(dut,seed,sync=False):
+    dut._log.info(f"Start Test: A nor B | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value | b_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
 
-async def test_a_b_c_nand_async(dut,seed):
-    dut._log.info("Start Test: A nand B nand C (Async)")
+async def test_a_b_c_nand(dut,seed,sync=False):
+    dut._log.info(f"Start Test: A nand B nand C | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value & b_value & c_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
 
-async def test_a_b_c_nor_async(dut,seed):
-    dut._log.info("Start Test: A nor B nor C (Async)")
+async def test_a_b_c_nor(dut,seed,sync=False):
+    dut._log.info(f"Start Test: A nor B nor C | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value | b_value | c_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
 
-async def test_a_b_c_majority_async(dut,seed):
-    dut._log.info("Start Test: 3 Bit Majority (Async)")
+async def test_a_b_c_majority(dut,seed,sync=False):
+    dut._log.info(f"Start Test: 3 Bit Majority | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (((a_value & b_value) | (c_value & (a_value | b_value)) ) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
 
-async def test_a_b_c_even_parity_async(dut,seed):
-    dut._log.info("Start Test: 3 Bit Even Parity (Async)")
+async def test_a_b_c_even_parity(dut,seed,sync=False):
+    dut._log.info(f"Start Test: 3 Bit Even Parity | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (~(a_value ^ b_value ^ c_value) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
 
-async def test_a_b_c_one_hot_async(dut,seed):
-    dut._log.info("Start Test: 3 Bit One Hot Detector (Async)")
+async def test_a_b_c_one_hot(dut,seed,sync=False):
+    dut._log.info(f"Start Test: 3 Bit One Hot Detector | Is Sync:{sync}")
     dut.uio_in.value = seed
-    dut.ui_in.value = 0b00001000
+    sync_mask = int(sync) << 4
+    sync_cycles = 2 if sync else 1
+    dut.ui_in.value = 0b00001000 | sync_mask
     await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00000000
+    dut.ui_in.value = 0b00000000 | sync_mask
     await ClockCycles(dut.clk,1)
     input_value = 0
     while(input_value != 8):
-        dut.ui_in.value = input_value
+        dut.ui_in.value = input_value | sync_mask
         a_value = input_value & 0b00000001
         b_value = (input_value & 0b00000010) >> 1
         c_value = (input_value & 0b00000100) >> 2
-        await ClockCycles(dut.clk,1)
+        await ClockCycles(dut.clk,sync_cycles)
         dut._log.info(f"A:{bin(a_value)} B:{bin(b_value)} C:{bin(c_value)} Out:{dut.uut.uo_out.value}")
         assert dut.uo_out.value == (((a_value & ~b_value & ~c_value) | (~a_value & b_value & ~c_value) | (~a_value & ~b_value & c_value)) & 0b00000001)
         input_value += 1
     await ClockCycles(dut.clk,1)
-
-async def test_a_not_sync(dut,seed):
-    dut._log.info("Start Test: not A (sync)")
-    dut.uio_in.value = seed
-    dut.ui_in.value = 0b00011000
-    await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 0b00010000
-    await ClockCycles(dut.clk,1)
-    input_value = 0
-    while(input_value != 8):
-        dut.ui_in.value = input_value | 0b00010000
-        a_value = input_value & 0b00000001
-        await ClockCycles(dut.clk,2)
-        dut._log.info(f"A:{bin(a_value)} Out:{dut.uut.uo_out.value}")
-        assert dut.uo_out.value == (~a_value & 0b00000001)
-        input_value += 1
-    await ClockCycles(dut.clk,2)
